a52f7d443a89f711d9ebe117e675e1a40c44dd5a
sindhusaravv <sindhusaravanan06@gmail.com>
2026-02-23 10:55:22 +0530

    UC3public class UseCase3PalindromeCheckerApp {:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,28 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
 
-        String word = "madam";
-        String reversedWord = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversedWord = reversedWord + word.charAt(i);
-        }
-        if (word.equals(reversedWord)) {
-            System.out.println("The word " + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The word " + word + "\" is NOT a Palindrome.");
-        }
-        System.out.println("Program Ended.");
-    }
 
+        public static void main(String[] args) {
+
+            String originalString = "level";
+
+            String reversedString = "";
+
+
+            for (int i = originalString.length() - 1; i >= 0; i--) {
+                reversedString = reversedString + originalString.charAt(i);
+            }
+
+
+            System.out.println("Original String : " + originalString);
+            System.out.println("Reversed String : " + reversedString);
+
+            if (originalString.equals(reversedString)) {
+                System.out.println("Result: It is a Palindrome.");
+            } else {
+                System.out.println("Result: It is NOT a Palindrome.");
+            }
+
+            System.out.println("Program Ended.");
+        }
 
 }
