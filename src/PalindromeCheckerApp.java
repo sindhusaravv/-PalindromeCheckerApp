c6227878f99a5dee8869c64c2bfab0e994591aa1
your GitHub username <your GitHub email>
2026-03-02 09:16:44 +0530

    UC5: stack based check
    :wq

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class PalindromeCheckerApp {
 
 
@@ -33,3 +34,36 @@ public class PalindromeCheckerApp {
         }
 
 }
+=======
+
+import java.util.Scanner;
+import java.util.Stack;
+
+     class UseCase5PalindromeCheckerApp {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Stack<Character> stack = new Stack<>();
+            System.out.println("Enter a string to check palindrome:");
+            String input = scanner.nextLine();
+
+            String cleanedInput = input.replaceAll("\\s+", "").toLowerCase();
+
+            for (int i = 0; i < cleanedInput.length(); i++) {
+                stack.push(cleanedInput.charAt(i));
+            }
+            String reversed = "";
+            while (!stack.isEmpty()) {
+                reversed = reversed + stack.pop();
+            }
+
+            if (cleanedInput.equals(reversed)) {
+                System.out.println("The given string is a Palindrome.");
+            } else {
+                System.out.println("The given string is NOT a Palindrome.");
+            }
+
+            scanner.close();
+        }
+    }
+
+>>>>>>> 96dacb8 (UC5: stack based check)
