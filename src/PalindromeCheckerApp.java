a30874d7a69b14df4991457073dfe39f57293271
your GitHub username <your GitHub email>
2026-03-02 10:25:07 +0530

    UC8: linked list based

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,36 +1,72 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
 
-public class UseCase7PalindromeCheckerApp {
+public class UseCase8PalindromeCheckerApp {
 
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        Deque<Character> deque = new ArrayDeque<>();
         System.out.println("Enter a string to check palindrome:");
         String input = scanner.nextLine();
-
         String cleanedInput = input.replaceAll("\\s+", "").toLowerCase();
-
-        for (int i = 0; i < cleanedInput.length(); i++) {
-            deque.addLast(cleanedInput.charAt(i));
+        if (cleanedInput.length() == 0) {
+            System.out.println("Empty string is a Palindrome.");
+            scanner.close();
+            return;
         }
-        boolean isPalindrome = true;
-
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
+        Node head = new Node(cleanedInput.charAt(0));
+        Node current = head;
+        for (int i = 1; i < cleanedInput.length(); i++) {
+            current.next = new Node(cleanedInput.charAt(i));
+            current = current.next;
         }
+        boolean isPalindrome = checkPalindrome(head);
         if (isPalindrome) {
             System.out.println("The given string is a Palindrome.");
         } else {
             System.out.println("The given string is NOT a Palindrome.");
         }
         scanner.close();
+    }
+    public static boolean checkPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+    public static Node reverse(Node head) {
+
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
     }
 }
