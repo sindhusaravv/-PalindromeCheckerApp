397ab6e39d3060f60f5fce75dfebc69f965d02bd
your GitHub username <your GitHub email>
2026-03-02 09:35:00 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,69 +1,41 @@
-<<<<<<< HEAD
-public class PalindromeCheckerApp {
-
-
-        public static void main(String[] args) {
-
-            String input = "radar";
-
-            char[] characters = input.toCharArray();
-
-            int start = 0;
-            int end = characters.length - 1;
-
-            boolean isPalindrome = true;
-
-            while (start < end) {
-                if (characters[start] != characters[end]) {
-                    isPalindrome = false;
-                    break;
-                }
-                start++;
-                end--;
-            }
-
-            System.out.println("Input String: " + input);
-
-            if (isPalindrome) {
-                System.out.println("Result: It is a Palindrome.");
-            } else {
-                System.out.println("Result: It is NOT a Palindrome.");
-            }
-
-            System.out.println("Program Ended.");
-        }
-
-}
-=======
-
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
+class UseCase6PalindromeCheckerApp {
 
-     class UseCase5PalindromeCheckerApp {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            Stack<Character> stack = new Stack<>();
-            System.out.println("Enter a string to check palindrome:");
-            String input = scanner.nextLine();
+    public static void main(String[] args) {
 
-            String cleanedInput = input.replaceAll("\\s+", "").toLowerCase();
+        Scanner scanner = new Scanner(System.in);
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+ System.out.println("Enter a string to check palindrome:");
+        String input = scanner.nextLine();
 
-            for (int i = 0; i < cleanedInput.length(); i++) {
-                stack.push(cleanedInput.charAt(i));
-            }
-            String reversed = "";
-            while (!stack.isEmpty()) {
-                reversed = reversed + stack.pop();
-            }
+        String cleanedInput = input.replaceAll("\\s+", "").toLowerCase();
 
-            if (cleanedInput.equals(reversed)) {
-                System.out.println("The given string is a Palindrome.");
-            } else {
-                System.out.println("The given string is NOT a Palindrome.");
-            }
-
-            scanner.close();
+        for (int i = 0; i < cleanedInput.length(); i++) {
+            char ch = cleanedInput.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
         }
-    }
 
->>>>>>> 96dacb8 (UC5: stack based check)
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            char fromQueue = queue.remove();
+            char fromStack = stack.pop();
+
+            if (fromQueue != fromStack) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+        scanner.close();
+    }
+}
